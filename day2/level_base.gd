1405182de840a79996061aaf79fd200af2cf968f
Leo Torrijos <holdstill04@gmail.com>
2025-02-09 13:24:00 -0500

    update level loading for day 2

--- day2/level_base.gd
+++ day2/level_base.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@export_file var next_level
+@export var next_level : PackedScene
 
 func _on_goal_player_win():
 	if next_level != null:
-		get_tree().change_scene_to_file(next_level)
+		get_tree().change_scene_to_packed_scene(next_level)
